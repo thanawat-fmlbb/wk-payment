298f669c999d2affa77d1b0c9ebe2a64aaf8f969
Nawat Ngerncham <67263644+nngerncham@users.noreply.github.com>
2023-11-23 18:19:13 +0700

    use hostnames and stuff from .env

--- src/database/__init__.py
+++ src/database/__init__.py
@@ -5,15 +5,18 @@ from sqlmodel import create_engine, SQLModel
 from src.database import models
 
 load_dotenv()
-DATABASE_HOST = os.environ.get("DATABASE_HOST", "localhost:5433")
+
+DATABASE_HOST = os.environ.get("DATABASE_HOST", "localhost")
+DATABASE_PORT = os.environ.get("DATABASE_PORT", "5433")
+
+DATABASE_NAME = os.environ.get("DATABASE_NAME", "postgres")
 DATABASE_USERNAME = os.environ.get("DATABASE_USERNAME", "postgres")
 DATABASE_PASSWORD = os.environ.get("DATABASE_PASSWORD", "password")
-DATABASE_NAME = os.environ.get("DATABASE_NAME", "postgres")
-DATABASE_URL = f"postgresql://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@{DATABASE_HOST}/{DATABASE_NAME}"
 
 
 def create_db_and_tables():
-    engine = create_engine(DATABASE_URL, echo=True)
+    postgres_url = f"postgresql://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
+    engine = create_engine(postgres_url, echo=True)
     SQLModel.metadata.create_all(engine)
 
 
