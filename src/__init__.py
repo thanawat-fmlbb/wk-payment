e5f387a8629f5acb0113e3cc494cb737e15ee701
Nawat Ngerncham <67263644+nngerncham@users.noreply.github.com>
2023-11-24 23:22:28 +0700

    updated db connection

--- src/__init__.py
+++ src/__init__.py
@@ -8,5 +8,5 @@ REDIS_HOST = os.environ.get("REDIS_HOST", "localhost")
 REDIS_PORT = os.environ.get("REDIS_PORT", "6381")
 app = Celery("payment_service",
              broker=f"redis://{REDIS_HOST}:{REDIS_PORT}/0",
-             backend=f"redis://{REDIS_HOST}:{REDIS_PORT}/0",
+             backend=f"redis://{REDIS_HOST}:{REDIS_PORT}/1",
              broker_connection_retry_on_startup=True)
--- src/database/__init__.py
+++ src/database/__init__.py
@@ -9,13 +9,14 @@ load_dotenv()
 DATABASE_HOST = os.environ.get("DATABASE_HOST", "localhost")
 DATABASE_PORT = os.environ.get("DATABASE_PORT", "5433")
 
-DATABASE_NAME = os.environ.get("DATABASE_NAME", "postgres")
 DATABASE_USERNAME = os.environ.get("DATABASE_USERNAME", "postgres")
 DATABASE_PASSWORD = os.environ.get("DATABASE_PASSWORD", "password")
 
 
 def create_db_and_tables():
-    postgres_url = f"postgresql://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
+    postgres_url = (f"postgresql://"
+                    f"{DATABASE_USERNAME}:{DATABASE_PASSWORD}@"
+                    f"{DATABASE_HOST}:{DATABASE_PORT}/payment")
     engine = create_engine(postgres_url, echo=True)
     SQLModel.metadata.create_all(engine)
 
