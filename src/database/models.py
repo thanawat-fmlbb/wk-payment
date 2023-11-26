61bd5efd2545f068c13c6a461b44ec549b0556ec
Nawat Ngerncham <67263644+nngerncham@users.noreply.github.com>
2023-11-26 17:02:30 +0700

    creates paymentInfo for each time a task is run

--- src/database/models.py
+++ src/database/models.py
@@ -14,6 +14,6 @@ class PaymentInfo(SQLModel, table=True):
     __tablename__ = "payment_info"
 
     id: int = Field(default=None, primary_key=True)
-    main_id: int
+    main_id: int  # doesn't need to be unique since we need to update the same "payment" at some point
     transaction_amount: int
     is_valid: bool
--- src/tasks.py
+++ src/tasks.py
@@ -3,29 +3,36 @@ from sqlalchemy.exc import SQLAlchemyError
 
 from src import app
 from src.database.engine import get_engine
-from src.database.models import UserMoney
+from src.database.models import UserMoney, PaymentInfo
 
 
 @app.task
 def create_payment(main_id, user_id: int, item_price: int, quantity: int) -> bool:
     engine = get_engine()
     with Session(engine) as session:
+        amount = item_price * quantity
         try:
             user = session.get(UserMoney, user_id)
             if user is None:
                 user = UserMoney(user_id=user_id)
                 session.add(user)
 
-            if user.money < item_price * quantity:
+            if user.money < amount:
                 session.rollback()
                 return False
 
-            user.money -= item_price * quantity
-            user.on_hold_money += item_price * quantity
+            user.money -= amount
+            user.on_hold_money += amount
+
+            payment = PaymentInfo(main_id=main_id, transaction_amount=amount, is_valid=True)
+            session.add(payment)
+
             session.commit()
             return True
         except SQLAlchemyError as e:
-            session.rollback()
+            payment = PaymentInfo(main_id=main_id, transaction_amount=amount, is_valid=False)
+            session.add(payment)
+            session.commit()
             return False
 
 
@@ -37,4 +44,4 @@ def rollback_payment(main_id, reason):
 if __name__ == '__main__':
     print(create_payment(0, 1, 12, 3))
     input()
-    print(create_payment(0, 1, 50, 3))
+    print(create_payment(1, 1, 50, 3))
