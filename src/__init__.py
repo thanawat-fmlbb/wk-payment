670470cea134bf1b00fea3f2a680e4f226129a66
Nawat Ngerncham <67263644+nngerncham@users.noreply.github.com>
2023-11-28 00:33:39 +0700

    updated redis channel

--- src/__init__.py
+++ src/__init__.py
@@ -7,6 +7,6 @@ create_db_and_tables()
 REDIS_HOST = os.environ.get("REDIS_HOST", "localhost")
 REDIS_PORT = os.environ.get("REDIS_PORT", "6381")
 app = Celery("payment_service",
-             broker=f"redis://{REDIS_HOST}:{REDIS_PORT}/0",
-             backend=f"redis://{REDIS_HOST}:{REDIS_PORT}/1",
+             broker=f"redis://{REDIS_HOST}:{REDIS_PORT}/2",
+             backend=f"redis://{REDIS_HOST}:{REDIS_PORT}/3",
              broker_connection_retry_on_startup=True)
